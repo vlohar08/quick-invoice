c8273907bad4e477eb4f653dd100b0421bc93f8e
Vishal Lohar <www.vlohar08@gmail.com>
2022-10-24 13:31:07 +0530

    Tailwind Fix and Favicon and Title added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import { InvoiceProvider } from "../context/InvoiceContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <InvoiceProvider>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <title>Quick Invoice - Simple and Quick Invoice Generator</title>
+      </Head>
       <Component {...pageProps} />
     </InvoiceProvider>
   );
