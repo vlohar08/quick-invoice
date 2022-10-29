4027591f6931c68c7cbfd4105c22151a505cb92d
Vishal Lohar <www.vlohar08@gmail.com>
2022-10-29 10:16:47 +0530

    Improved Speed - Lazy Loading Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,7 +8,7 @@ import SendInvoiceBtn from "./SendInvoiceBtn";
 const Sidebar = () => {
   const invoice = useInvoice();
   return (
-    <aside className="w-full bg-white p-5 rounded-lg flex flex-col xl:w-1/5">
+    <aside className="w-full flex flex-col">
       <SendInvoiceBtn />
       <div className="flex justify-between my-3 flex-wrap gap-3">
         <DefaultButton
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import dynamic from "next/dynamic";
+import LazyLoad from "react-lazyload";
 import InvoiceForm from "../components/InvoiceForm";
-import Sidebar from "../components/Sidebar";
+const Sidebar = dynamic(() => import("../components/Sidebar"));
 
 export default function Home() {
   return (
@@ -8,7 +10,13 @@ export default function Home() {
         <main className="rounded-lg min-h-[90vh] bg-white w-full xl:w-4/5 xl:max-w-[1000px] p-5">
           <InvoiceForm />
         </main>
-        <Sidebar />
+        <LazyLoad
+          once
+          className="w-full bg-white p-5 rounded-lg xl:w-1/5"
+          offset={100}
+        >
+          <Sidebar />
+        </LazyLoad>
       </section>
     </div>
   );
